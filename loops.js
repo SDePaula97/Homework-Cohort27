4d858047288364ac39dc50bc5a548b3f60740004
Svetly de Paula <67973110+SDePaula97@users.noreply.github.com>
2022-08-24 16:17:44 -0400

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,59 @@
+
+
+
+1// for(let i = 10; i >= 0; i--){
+//  if (let i % 2 !=== 0)
+// console.log(i);
+// }
+
+2// for (let i=1; 1 <=10; i+=2){
+//     console.log(`${i}`);
+// }
+// for (let i = 2; i <= 10; i+=2){
+//     console.log(`${i}`);
+// }
+
+
+
+
+3// for (let i = 6; i <= 60; i++){
+//     if (i % 3 == 0){
+//         console.log(`${i} is a multiple of 3`)
+//     }
+// }
+
+
+
+4// for (let i = 0; i < 11; i++){
+//     if(i %2 == 0){
+//         console.log(`${i} is even`)
+//     }
+// }
+
+5// const word = '#######';
+// let secondWord = ``;
+// for (let i = 0; i < word.length; i++) {
+//     secondWord =+ word[i];
+//     console.log(secondWord);
+// }
+
+
+6// for (let i = 0; i < 35; i++){
+//     if (i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+
+7// for (let i = 0; i < 100; i++){
+//     if (i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+8// for (let i = 0; i < 21; i++){
+//     if (i % 2 == 0){
+//         let num = 'i * 3';
+//         console.log(i);
+//         console.log(num);
+//     }
+// }
