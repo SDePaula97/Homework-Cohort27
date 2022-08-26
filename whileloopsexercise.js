08e040f36fa922a5e61f15748e41335a57ff638d
Svetly de Paula <67973110+SDePaula97@users.noreply.github.com>
2022-08-26 14:36:17 -0400

    Add files via upload

---
+++ whileloopsexercise.js
@@ -0,0 +1,31 @@
+
+
+1// Use a while loop to increment count by 2 on each repetition of the block of code. Run the code block of your while loop until count is 8.
+
+let count = 2;
+// ADD CODE HERE
+while(count <= 8){
+    console.log(count);
+    count += 2
+}
+
+
+2// Initialize a variable addThis to 0 and a variable sum to 0. 
+// Use a while loop to repeat a code block as long as addThis is less than 10. 
+// In the code block, add the value of addThis to sum, then increment addThis by 1. 
+// After the while loop runs, the value of sum should be the sum of the numbers 0 through 9.
+
+ 
+
+// ADD CODE HERE
+let addThis = 0
+let sum = 0
+
+while(addThis < 10){
+    
+    sum += addThis;
+    addThis++;
+   
+}
+console.log(sum);
+
